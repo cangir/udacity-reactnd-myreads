e1baf6dd691264c8cbe80139a1874271cd8795b9
cangir <ahmetcangir07@gmail.com>
2019-10-03 03:55:59 +0200

    Updates

---
+++ precache-manifest.2fa946be0fad0e1636bf33c4b5105079.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec44d4c9452225d4be58aa8324dc8cf",
+    "url": "/udacity-reactnd-myreads/index.html"
+  },
+  {
+    "revision": "24b02021ff28a1b349a9",
+    "url": "/udacity-reactnd-myreads/static/css/main.5abe2c9a.chunk.css"
+  },
+  {
+    "revision": "eb185498edb146e0079d",
+    "url": "/udacity-reactnd-myreads/static/js/2.35f435ac.chunk.js"
+  },
+  {
+    "revision": "24b02021ff28a1b349a9",
+    "url": "/udacity-reactnd-myreads/static/js/main.6179ea15.chunk.js"
+  },
+  {
+    "revision": "038916c489df0aeef35d",
+    "url": "/udacity-reactnd-myreads/static/js/runtime-main.2b5f7544.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/udacity-reactnd-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/udacity-reactnd-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "8297cd8d2ffe4f0a6ce859fadf738e41",
+    "url": "/udacity-reactnd-myreads/static/media/search-solid.8297cd8d.svg"
+  }
+]);
